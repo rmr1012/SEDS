138cc4dc112202b212c721c5a346ef6901300a92
Rohit Ghosh <Rohit@Rohits-MacBook-Pro.local>
2016-01-16 14:25:54 -0800

    Added files

--- testSfsCode.c
+++ testSfsCode.c
@@ -1,5 +1,77 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1404\cocoasubrtf340
-{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+/*
+ * Name: testSfsCode
+ * Description: Very intial code. Mostly pseudo-code.
+ */
+
+// IF (YOU ARE NOT A DEVELOPER) {READ ONLY THE COMMENTS;}
+// THIS IS A COMMENT
+/* THIS IS A COMMENT TOO*/
+
+//Header files
+
+#include <stdio.h>  //Std io
+#incldue <time.h>   //For clock()
+
+//Assign registers names
+TODO
+
+
+//Helper methods
+
+void actuate(boolean valve) {
+    valve = !(valve);
+}
+
+//main()
+
+int main() {
+    clock_t start_t, end_t; //Declare clock variables
+    
+    //Add actuations to be done manually
+    //Get input command
+    //Actuation based on input
+    while((c = getChar()) != 'x') {
+        switch (c) {
+            case 'a':
+                //Do something
+            //Fill out other cases
+                
+            default:
+                break;
+        }
+    }
+    
+    
+    //START OF TIMED SEQUENCE
+    
+    start_t = clock();
+    
+    while(1) {
+        switch (clock()) {
+            case 1:
+                //Do whatever happens in 1 sec.
+            case 2:
+                //Do whatever happens in 2 sec.
+            case 3:
+                //Do whatever happens in 3 sec.
+            case 4:
+                //Do whatever happens in 4 sec.
+            case 5:
+                //Do whatever happens in 5 sec.
+            case 6:
+                //Do whatever happens in 6 sec.
+            case 7:
+                //Do whatever happens in 7 sec.
+            case 8:
+                //Do whatever happens in 8 sec.
+            case 9:
+                //Do whatever happens in 9 sec.
+            case 10:
+                //Do whatever happens in 10 sec.
+                
+            default:
+                break;
+        }
+    }
+    return 0;
 }
